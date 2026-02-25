2cc7c910b3828673538136a3d6e4bcbb6e932d0a
anabelcampues24-commits <anabelcampues24@gmail.com>
2026-02-24 20:54:12 -0500

    Add files via upload

--- juegoahorcado.py
+++ juegoahorcado.py
@@ -1,7 +1,7 @@
 # INICIO
 
 # Palabra secreta
-palabra_secreta = "python"
+palabra_secreta = "anabel"
 
 # Lista para guardar letras ingresadas
 letras_adivinadas = []
@@ -9,9 +9,9 @@ letras_adivinadas = []
 # Número máximo de intentos
 intentos = 6
 
-print("=================================")
+print("////////////////////////////////////")
 print("   BIENVENIDO AL JUEGO DEL AHORCADO")
-print("=================================")
+print("////////////////////////////////////")
 
 # CICLO PRINCIPAL DEL JUEGO
 while True:
@@ -28,9 +28,34 @@ while True:
     print("\nPalabra:", palabra_mostrada)
     print("Intentos restantes:", intentos)
 
-    # DECISIÓN: ¿La palabra está completa?
+    # CONDICION: ¿La palabra está completa?
     if "_" not in palabra_mostrada:
         print("\n ¡FELICIDADES, GANASTE!")
         break
 
-    
+    # CONDICION: ¿Se acabaron los intentos?
+    if intentos == 0:
+        print("\n ¡PERDISTE!")
+        print("La palabra secreta era:", palabra_secreta)
+        break
+
+    # ENTRADA: Ingresar una letra
+    letra_ingresada = input("\nIngresa una letra: ").lower()
+
+    # VALIDACIÓN: letra repetida
+    if letra_ingresada in letras_adivinadas:
+        print(" Ya ingresaste esa letra, intenta otra.")
+        continue
+
+    # Guardar la letra ingresada
+    letras_adivinadas.append(letra_ingresada)
+
+    # CONDICIO: ¿La letra está en la palabra?
+    if letra_ingresada in palabra_secreta:
+        print(" Letra correcta")
+    else:
+        print(" Letra incorrecta")
+        intentos -= 1
+
+# FIN DEL PROGRAMA
+print("\nGracias por jugar ")
